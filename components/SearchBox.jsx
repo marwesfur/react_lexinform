ffdf463de5da0a3ce7d04ccaa92eb2052e40cb77
Markus Westphal <mail@markuswestphal.de>
2016-04-11 21:53:18 +0200

    enter query

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 export default React.createClass({
 
+	getInitialState() {
+		return {query: ''}
+	},
+
+	onQueryChanged(e) {
+		this.setState({query: e.target.value})
+	},
+
 	render: function() {
 		return (
 			<div>
-				<input type="text" />
+				<input type="text" value={this.state.query} onChange={this.onQueryChanged} />
 				<button>Suchen</button>
+				{this.state.query}
 			</div>
 		);
 	}
